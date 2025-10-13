efd92aaa222fd81a0654f00237214ea3f3e97f9f
SonPhamBOW <2k3sonpham@gmail.com>
2025-10-13 16:01:20 +0700

    Movie detail fix

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,6 @@
 // export const BASE_URL = "https://bookmovie-5n6n.onrender.com";
-export const BASE_URL = "https://localhost:7263";
+export const BASE_URL =
+  "https://expressticketcinemasystemapi-fjescsgjg9djeuf5.southeastasia-01.azurewebsites.net";
 
 export const movieCategoryQuickAccess = [
   {
